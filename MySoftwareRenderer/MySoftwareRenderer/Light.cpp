e4aa351ee8401066667c6ee8940fc12217167ca4
coreyao <yjh1023@qq.com>
2015-10-19 22:41:33 +0800

    add

---
+++ MySoftwareRenderer/MySoftwareRenderer/Light.cpp
@@ -0,0 +1,92 @@
+#include "Light.h"
+#include "Image/ImageManager.h"
+#include "Mesh.h"
+
+CDirectionalLight::CDirectionalLight()
+: CLightBase()
+{
+	m_eLightType = ELightType_DirectionalLight;
+}
+
+CLightManager* CLightManager::s_pInstance = nullptr;
+CLightManager* CLightManager::GetInstance()
+{
+	if (!s_pInstance)
+	{
+		s_pInstance = new CLightManager;
+		s_pInstance->m_vAllDirectionalLight.resize(conMaxDirectionalLightNum);
+		s_pInstance->m_vAllPointLight.resize(conMaxPointLightNum);
+		s_pInstance->m_vAllSpotLight.resize(conMaxSpotLightNum);
+	}
+
+	return s_pInstance;
+}
+
+void CLightManager::AddLight(CLightBase* pLight)
+{
+	switch (pLight->m_eLightType)
+	{
+	case ELightType_DirectionalLight:
+	{
+										if (m_iCurDirectionalLightNum < conMaxDirectionalLightNum)
+											m_vAllDirectionalLight[m_iCurDirectionalLightNum++] = *(CDirectionalLight*)pLight;
+	}
+		break;
+	case ELightType_PointLight:
+	{
+								  if (m_iCurPointLightNum < conMaxDirectionalLightNum)
+									  m_vAllPointLight[m_iCurPointLightNum++] = *(CPointLight*)pLight;
+	}
+		break;
+	case ELightType_SpotLight:
+	{
+								 if (m_iCurSpotLightNum < conMaxSpotLightNum)
+									 m_vAllSpotLight[m_iCurSpotLightNum++] = *(CSpotLight*)pLight;
+	}
+		break;
+	default:
+		break;
+	}
+}
+
+const std::vector<CDirectionalLight>& CLightManager::GetAllDirectionalLights()
+{
+	return m_vAllDirectionalLight;
+}
+
+const std::vector<CPointLight>& CLightManager::GetAllPointLights()
+{
+	return m_vAllPointLight;
+}
+
+const std::vector<CSpotLight>& CLightManager::GetAllSpotLights()
+{
+	return m_vAllSpotLight;
+}
+
+CLightManager::CLightManager()
+: m_iCurDirectionalLightNum(0)
+, m_iCurPointLightNum(0)
+, m_iCurSpotLightNum(0)
+{
+}
+
+CPointLight::CPointLight()
+: CLightBase()
+, m_attenuation_constant(1.0f)
+, m_attenuation_linear(0.0f)
+, m_attenuation_quadratic(0.0f)
+{
+	m_eLightType = ELightType_PointLight;
+}
+
+CSpotLight::CSpotLight()
+: CLightBase()
+, m_attenuation_constant(1.0f)
+, m_attenuation_linear(0.0f)
+, m_attenuation_quadratic(0.0f)
+, fInnerAngle(0.0f)
+, fOuterAngle(0.0f)
+{
+	m_eLightType = ELightType_SpotLight;
+}
---
+++ MySoftwareRenderer/MySoftwareRenderer/Light.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "Utility.h"
+
+enum ELightType
+{
+	ELightType_DirectionalLight,
+	ELightType_PointLight,
+	ELightType_SpotLight,
+};
+
+class CMesh;
+class CLightBase
+{
+public:
+	ELightType m_eLightType;
+
+	Vec3 m_ambientColor;
+	Vec3 m_diffuseColor;
+	Vec3 m_specularColor;
+
+	Vec3 m_lightDir;
+	Vec3 m_lightPos;
+	bool m_bOn;
+
+	CMesh* m_pDebugMesh;
+
+protected:
+	CLightBase()
+		: m_lightDir(Vec3(0, 0, -1))
+		, m_bOn(true)
+		, m_pDebugMesh(nullptr)
+		, m_eLightType(ELightType_DirectionalLight)
+	{
+	}
+};
+
+class CDirectionalLight : public CLightBase
+{
+public:
+	CDirectionalLight();
+};
+
+class CPointLight : public CLightBase
+{
+public:
+	CPointLight();
+
+	float m_attenuation_constant;
+	float m_attenuation_linear;
+	float m_attenuation_quadratic;
+};
+
+class CSpotLight : public CLightBase
+{
+public:
+	CSpotLight();
+
+	float fInnerAngle;
+	float fOuterAngle;
+
+	float m_attenuation_constant;
+	float m_attenuation_linear;
+	float m_attenuation_quadratic;
+};
+
+class CLightManager
+{
+public:
+	static CLightManager* GetInstance();
+
+	void AddLight(CLightBase* pLight);
+
+	const std::vector<CDirectionalLight>& GetAllDirectionalLights();
+	const std::vector<CPointLight>& GetAllPointLights();
+	const std::vector<CSpotLight>& GetAllSpotLights();
+
+	static const int conMaxDirectionalLightNum = 5;
+	static const int conMaxPointLightNum = 5;
+	static const int conMaxSpotLightNum = 5;
+
+private:
+	static CLightManager* s_pInstance;
+
+	CLightManager();
+
+	int m_iCurDirectionalLightNum;
+	std::vector<CDirectionalLight> m_vAllDirectionalLight;
+
+	int m_iCurPointLightNum;
+	std::vector<CPointLight> m_vAllPointLight;
+
+	int m_iCurSpotLightNum;
+	std::vector<CSpotLight> m_vAllSpotLight;
+};
