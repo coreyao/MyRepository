02735e76f0964c7102828a0b55187b168ec261bc
coreyao <yjh1023@qq.com>
2015-11-12 16:47:42 +0800

    delete

--- YDK/Source/Core/Utility/Precomp/win32/Precomp_win32.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_UTILITY_PRECOMP_WIN32_H__INCLUDED
-#define CORE_UTILITY_PRECOMP_WIN32_H__INCLUDED
-
-#pragma message("--- Start config Win32 version")
-#include <windows.h>
-#include <process.h>
-#pragma message("--- End config Win32 version  ")
-
-#endif
